67ab25aad09242c34d21551a942b1bfcbcafcc07
tolgaze <tzeybek90@gmail.com>
2023-10-14 20:20:12 +0300

    test

--- src/main/java/com/tolgaze/financialreportbff/config/FirestoreConfig.java
+++ src/main/java/com/tolgaze/financialreportbff/config/FirestoreConfig.java
@@ -5,15 +5,25 @@ import java.io.IOException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.FirestoreOptions;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
 
 @Configuration
 public class FirestoreConfig {
 
   @Bean
   Firestore firestore() throws IOException{
-		return FirestoreOptions.getDefaultInstance().getService();
+    GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
+    FirebaseOptions options = FirebaseOptions.builder()
+      .setCredentials(credentials)
+      .setProjectId("ageless-thought-400117")
+      .build();
+    FirebaseApp.initializeApp(options);
+
+		return FirestoreClient.getFirestore();
   }
   
 }
